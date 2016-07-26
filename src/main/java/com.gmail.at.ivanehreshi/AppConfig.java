ad0f788c1bec0430b8e7576a8787ef1e04332f30
ivaneh <ivanehreshi@gmail.com>
2016-07-26 18:05:02 +0300

    Set up spring context

--- src/main/java/com.gmail.at.ivanehreshi/AppConfig.java
+++ src/main/java/com.gmail.at.ivanehreshi/AppConfig.java
@@ -1,13 +1,21 @@
 package com.gmail.at.ivanehreshi;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-public class AppConfig
-        implements InitializingBean {
+@ImportResource("classpath:/context.xml")
+public class AppConfig implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("AppConfig initialized");
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx =
+                new AnnotationConfigApplicationContext(AppConfig.class);
     }
 }

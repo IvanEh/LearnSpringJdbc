1d90413b18b2877f0a0332495e7a42156ace52e2
ivaneh <ivanehreshi@gmail.com>
2016-07-28 19:28:14 +0300

    Remove redundant code

--- src/main/java/com.gmail.at.ivanehreshi/UserDao.java
+++ src/main/java/com.gmail.at.ivanehreshi/UserDao.java
@@ -23,56 +23,14 @@ import java.util.Optional;
 // be managed by Spring
 @Repository
 public class UserDao implements Dao<User, Integer> {
-    // JdbcTemplate is the core class for easily using jdbc
-    // There are also such useful classes as: NamedParameterJdbcTemplate, SimpleJdbcInsert, SimpleJdbcCall
     @Autowired
     NamedParameterJdbcTemplate jdbc;
 
-    // Three usages of NamedParameterJdbcTemplate. Uncomment to test
-    // Parameters introduced by ':' symbol
     @Override
     public Integer create(User user) {
-        return createWithJavaMap(user); // This actually always return null
-//        return createWithKeyHolderAndMapParameterSource(user);
-//        return createWithBeanPropertySqlParameterSource(user);
+        return createWithBeanPropertySqlParameterSource(user);
     }
 
-    // There is a Map<String, Object> used for substituting parameters
-    // Limitation: cannot pass KeyHolder object to retrieve row ids
-    private Integer createWithJavaMap(User user) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("age", user.age);
-        params.put("name", user.name);
-
-        jdbc.update("INSERT INTO user (name, age) VALUES(:name, :age)", params);
-        return null;
-    }
-
-    // First example of SqlParameterSource usage
-    //
-    // From docs: Interface that defines common functionality for objects that can offer
-    // parameter values for named SQL parameters, serving as argument for
-    // NamedParameterJdbcTemplate operations.
-    //
-    // Generally it is the previous solution but with the usage of more powerful
-    // SqlParameterSource. The particular impl MapSqlParameterSource uses a Java
-    // Map<String, Object> map to provide parameter values
-    //
-    // Method signatures that uses SqlParameterSource allows to pass KeyHolder object
-    // which will store row ids once the query is executed
-    private Integer createWithKeyHolderAndMapParameterSource(User user) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("age", user.age);
-        params.put("name", user.name);
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        SqlParameterSource parameterSource = new MapSqlParameterSource(params);
-        jdbc.update("INSERT INTO user (name, age) VALUES(:name, :age)", parameterSource, keyHolder);
-        return Optional.ofNullable(keyHolder.getKey()).orElse(-1).intValue();
-    }
-
-    // BeanPropertySqlParameterSource a particularly useful impl of SqlParameterSource
-    // that automatically maps bean properties to parameter properties
     private Integer createWithBeanPropertySqlParameterSource(User user) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
@@ -90,10 +48,6 @@ public class UserDao implements Dao<User, Integer> {
                                                         , user.id );
     }
 
-    // The lambda is implementing RowMapper interface
-    // in Spring Jdbc many manipulation are done via functional interfaces
-    // EmptyResultDataAccessException is thrown when there is no data
-    // More on this will be later
     @Override
     public User read(Integer id) {
         try {
@@ -107,7 +61,6 @@ public class UserDao implements Dao<User, Integer> {
         }
     }
 
-    // nothing changed
     @Override
     public void delete(Integer id) {
         jdbc.getJdbcOperations().update("DELETE FROM user WHERE id=?", id);
